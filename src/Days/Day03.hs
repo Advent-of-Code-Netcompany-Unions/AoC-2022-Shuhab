05f295fb761f2f391c1057b84ed500605d86080b
Shuhab Hussain <shuhab@gmail.com>
2022-12-04 23:42:05 +0100

    Solve for Day 3

--- src/Days/Day03.hs
+++ src/Days/Day03.hs
@@ -3,6 +3,7 @@ module Days.Day03 (runDay) where
 {- ORMOLU_DISABLE -}
 import Data.List
 import Data.Map.Strict (Map)
+import Data.Char
 import qualified Data.Map.Strict as Map
 import Data.Maybe
 import Data.Set (Set)
@@ -21,19 +22,35 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = many1 letter `sepBy` endOfLine
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [String]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
+priority :: Char -> Int
+priority x = 
+    if
+        | isAlpha x && isLower x -> ord x - 96 -- a-z is 1 to 26
+        | isAlpha x && isUpper x -> ord x - 38 -- A-Z is 27 to 52
+        | otherwise -> error "Illegal input character"
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = sum . fmap (priority . uncurry shared . (\s -> splitAt (length s `div` 2) s))
+    where
+        shared l r = Set.elemAt 0 $ Set.fromList l `Set.intersection` Set.fromList r
+             
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = findBadges
+    where
+        findBadges [] = 0
+        findBadges (x:y:z:ss) = 
+            findBadges ss 
+            + (priority . Set.elemAt 0 . foldl1' Set.intersection $ 
+                [Set.fromList x, Set.fromList y, Set.fromList z])
