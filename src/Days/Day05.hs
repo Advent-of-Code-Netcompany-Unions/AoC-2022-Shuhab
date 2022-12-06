c888533fe1ece9e594433176e3aaf9d127ed9a86
Shuhab Hussain <shuhab@hey.com>
2022-12-06 21:39:11 +0100

    Solve for Day 5

--- src/Days/Day05.hs
+++ src/Days/Day05.hs
@@ -1,10 +1,12 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE BlockArguments #-}
 module Days.Day05 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
+import Data.List ( transpose )
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
-import Data.Maybe
+import Data.Maybe ( catMaybes )
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Vector (Vector)
@@ -13,7 +15,21 @@ import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
+    ( count,
+      sepBy,
+      sepBy1,
+      decimal,
+      letter,
+      space,
+      endOfLine,
+      takeTill,
+      Parser )
 import Data.Void
+import Control.Applicative ( Alternative((<|>)), liftA3 )
+import Data.Functor ( ($>), (<&>) )
+import Data.Foldable
+import Data.Function
+import Debug.Trace
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +37,31 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = let
+  crate = ("[" *> letter <* "]" <&> Just) <|> (count 3 space $> Nothing)
+  line = liftA3 (,,) ("move " *> decimal) (" from " *> decimal) (" to " *> decimal)
+  in do
+  crates <- Map.fromList . zip [1..] . fmap catMaybes . transpose <$> crate `sepBy` " " `sepBy` endOfLine
+  takeTill (== 'm')
+  lines <- line `sepBy1` endOfLine
+  pure (crates, lines)        
 
 ------------ TYPES ------------
-type Input = Void
+type Input = (Map Int [Char], [(Int, Int, Int)]) 
 
-type OutputA = Void
+type OutputA = String
 
-type OutputB = Void
+type OutputB = String
 
 ------------ PART A ------------
+doMoves :: (Foldable t, Ord k) => ([a] -> [a]) -> Map k [a] -> t (Int, k, k) -> Map k [a]
+doMoves f = foldl' \piles (n,x,y) ->
+  let p = f $ take n $ piles Map.! x
+  in piles & Map.adjust (drop n) x & Map.adjust (p ++) y
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = map head . Map.elems . uncurry (doMoves reverse)
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = map head . Map.elems . uncurry (doMoves id)
